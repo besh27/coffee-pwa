5d884b7e84b80df6fb5dc64cd629b3d2d206c510
besh27 <beshdevelopment@gmail.com>
2020-02-01 06:46:39 -0600

    Add service worker and fix formatting

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+    { name: "Perspiciatis", image: "images/coffee1.jpg" },
+    { name: "Voluptatem", image: "images/coffee2.jpg" },
+    { name: "Explicabo", image: "images/coffee3.jpg" },
+    { name: "Rchitecto", image: "images/coffee4.jpg" },
+    { name: " Beatae", image: "images/coffee5.jpg" },
+    { name: " Vitae", image: "images/coffee6.jpg" },
+    { name: "Inventore", image: "images/coffee7.jpg" },
+    { name: "Veritatis", image: "images/coffee8.jpg" },
+    { name: "Accusantium", image: "images/coffee9.jpg" },
 ]
 
 const showCoffees = () => {
     let output = ""
     coffees.forEach(
-      ({ name, image }) =>
-        (output += `
+        ({ name, image }) =>
+            (output += `
                 <div class="card">
                   <img class="card--avatar" src=${image} />
                   <h1 class="card--title">${name}</h1>
@@ -24,6 +24,15 @@ const showCoffees = () => {
                 `)
     )
     container.innerHTML = output
-  }
-  
-  document.addEventListener("DOMContentLoaded", showCoffees)
+}
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees)
